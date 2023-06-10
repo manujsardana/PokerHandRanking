438b4b5c5396c2edc2f193b78cacc9e38e1462ed
Manuj Sardana <manuj.sardana@gmail.com>
2023-06-10 13:04:41 +0200

    Initial commit with no functionality

---
+++ PokerHandRanking.Tests/PokerHandRankingTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PokerHandRanking.Tests
+{
+    [TestClass]
+    public class PokerHandRankingTests
+    {
+        private PokerHandRanking pokerHandRanking;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            pokerHandRanking = new PokerHandRanking();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            pokerHandRanking = null;
+        }
+        
+        [TestMethod]
+        public void PokerHandRanking_returns_royal_flush()
+        {
+            var hand = new List<Card>()
+            {
+                new Card() { Rank = Rank.Ace, Suit = Suit.Club },
+                new Card() { Rank = Rank.King, Suit = Suit.Club },
+                new Card() { Rank = Rank.Queen, Suit = Suit.Club },
+                new Card() { Rank = Rank.Jack, Suit = Suit.Club },
+                new Card() { Rank = Rank.Ten, Suit = Suit.Club }
+            };
+
+            var result = pokerHandRanking.RankHand(hand);
+
+            Assert.AreEqual("Royal Flush", result);
+        }
+    }
+}
---
+++ PokerHandRanking/PokerHandRanking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokerHandRanking
+{
+    public class PokerHandRanking
+    {
+        public string RankHand(List<Card> hand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
